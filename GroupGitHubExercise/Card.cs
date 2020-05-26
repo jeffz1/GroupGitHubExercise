be283587b9b548e9d8eccec8e0db9b5ac4944d2b
jeffz1 <jm@jeffz.com>
2020-05-26 14:25:27 -0400

    Added Card class  to represent a standard deck of playing cards

---
+++ GroupGitHubExercise/Card.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupGitHubExercise
+{
+    public class Card
+    {
+        public readonly static string[] Faces = { "Ace", "Two", "Three", "Four", "Five", "Six",
+                               "Seven", "Eight", "Nine", "Ten",
+                               "Jack", "Queen", "King"};
+        public readonly static string[] Suits = { "Donkeys", "Camels", "Horses", "Snakes" };
+
+        private int face;
+        public int Face
+        {
+            get { return face; }
+            private set
+            {
+                if (value < 0 || value > Faces.Length)
+                    throw new Exception("Invalid value for Face");
+                face = value;
+            }
+        }
+
+        private int suit;
+        public int Suit
+        {
+            get { return suit; }
+            private set
+            {
+                if (value < 0 || value > Suits.Length)
+                    throw new Exception("Invalid value for Suit");
+                suit = value;
+            }
+        }
+
+    }
+}
