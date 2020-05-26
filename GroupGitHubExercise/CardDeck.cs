98d468fdb688a798634b50cce18aeb43a884f945
jeffz1 <jm@jeffz.com>
2020-05-26 14:33:33 -0400

    Added CardDeck class

---
+++ GroupGitHubExercise/CardDeck.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GroupGitHubExercise
+{
+    class CardDeck
+    {
+        private Card[] Deck { get; set; }
+        private int currentCard;
+        private Random _rnd = new Random();
+
+        public CardDeck()
+        {
+            Deck = new Card[Card.Faces.Length * Card.Suits.Length];
+            currentCard = 0;
+
+
+            int cardIndex = 0;
+            for (int suit = 0; suit < 3; suit++)
+            {
+                for (int face = 0; face < 11; face++)
+                {
+                    Deck[cardIndex++] = new Card(face, suit);
+                }
+            }
+        }
+    }
+}
