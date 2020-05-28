c2e9be208f670d9e675f35e67c2b983f8224caf4
AshofBoca <ash_systems@yahoo.com>
2020-05-27 21:30:04 -0400

    Fixed the problem in Shuffle Method of CardDeck class

--- GroupGitHubExercise/CardDeck.cs
+++ GroupGitHubExercise/CardDeck.cs
@@ -46,7 +46,8 @@ namespace GroupGitHubExercise
             {
                 int randomIndex = _rnd.Next(0, Deck.Length);
                 Card temp = Deck[card];
-                Deck[card] = temp;
+                //Deck[card] = temp; // This will cause duplicates
+                Deck[card] = Deck[randomIndex];
                 Deck[randomIndex] = temp;
             }
         }
